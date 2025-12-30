a0babcc3f9d6c5eb2f144ded16a72841a6b6ad5b
safetysquad <ddenisdill96@gmail.com>
2025-12-30 14:03:54 +0100

    Create pruefungData.js

---
+++ pruefungData.js
@@ -0,0 +1,13 @@
+const pruefungsFragen = [
+  {
+    id: 1,
+    frage: "Das Recht auf freie Entfaltung der Persönlichkeit (Art. 2 GG) …?",
+    optionen: [
+      "beinhaltet das informationelle Selbstbestimmungsrecht",
+      "ist ein Bürgerrecht",
+      "ist ein Menschenrecht",
+      "darf niemals eingeschränkt werden"
+    ],
+    korrekt: [0, 2]
+  }
+];
