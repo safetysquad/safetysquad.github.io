c64a49db06bb6da74b37c28b8ff394a2bc980b1c
safetysquad <ddenisdill96@gmail.com>
2026-01-22 08:40:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 // ===============================
 // === INITIAL STATE & DATENSET ===
 // ===============================
-const currentSet = document.body.dataset.set;
-const cardsActive = { cards, cards1, cards2 }[currentSet] || cards;
+const currentSet = document.body.dataset.set; // "cards", "cards1", "cards2"
+
+const cardsActive =
+  currentSet === "cards1"
+    ? cards1
+    : currentSet === "cards2"
+    ? cards2
+    : cards;
+
 
 
 let currentIndex = 0;
